896baae611b35069aa063b99a4e51a87e2c56467
Navaneeth Venu <69888461+navaneethvenu@users.noreply.github.com>
2024-07-25 01:50:36 +0530

    feat: add setPositionProperty command

---
+++ src/plugin/set-properties/utils/set-pos.ts
@@ -0,0 +1,67 @@
+import notifyError from '../../utils/error';
+import { ErrorType } from '../../utils/errorType';
+
+interface setPositionProps {
+  param: string;
+  value: string;
+  node: FrameNode | PolygonNode | RectangleNode | EllipseNode | StarNode | LineNode | VectorNode;
+}
+
+export default function setPosition({ param, value, node }: setPositionProps) {
+  console.log(value);
+  const position = parseFloat(value);
+  const nodeTypeCheck =
+    node.type === 'FRAME' ||
+    node.type === 'RECTANGLE' ||
+    node.type === 'POLYGON' ||
+    node.type === 'ELLIPSE' ||
+    node.type === 'STAR' ||
+    node.type === 'VECTOR' ||
+    node.type === 'LINE';
+
+  if (nodeTypeCheck) {
+    if (!isNaN(position)) {
+      //X Position
+      if (/posx\b/.test(param)) {
+        if (node.parent.type === 'FRAME' && node.parent.layoutMode !== null) node.layoutPositioning = 'ABSOLUTE';
+        node.x = position;
+      }
+
+      //Y Position
+      else if (/posy\b/.test(param)) {
+        if (node.parent.type === 'FRAME' && node.parent.layoutMode !== null) node.layoutPositioning = 'ABSOLUTE';
+        node.y = position;
+      }
+
+      //All Strokes
+      else if (/pos\b/.test(param)) {
+        if (node.parent.type === 'FRAME' && node.parent.layoutMode !== null) node.layoutPositioning = 'ABSOLUTE';
+        node.x = position;
+        node.y = position;
+      }
+
+      //Invalid Command
+      else {
+        notifyError({
+          type: ErrorType.INVALID_CMD,
+          message: param,
+        });
+      }
+    }
+
+    //Invalid Value
+    else {
+      notifyError({
+        type: ErrorType.INVALID_VAL,
+        message: param,
+      });
+    }
+  }
+  //Unsupported Prop
+  //   else {
+  //     notifyError({
+  //       type: ErrorType.UNSUPPORTED_PROP,
+  //       message: `Position is not applicable on node type ${node.type}`,
+  //     });
+  //   }
+}
