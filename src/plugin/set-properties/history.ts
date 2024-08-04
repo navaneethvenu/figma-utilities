216140eb406f4c0171135cefac973468da95562f
Navaneeth Venu <69888461+navaneethvenu@users.noreply.github.com>
2024-08-04 18:46:47 +0530

    refact: remove unecessary logs

--- src/plugin/set-properties/history.ts
+++ src/plugin/set-properties/history.ts
@@ -1,15 +1,12 @@
 import { ParsedParameter } from './parse-params';
 
 export async function addHistory(parsedParams: ParsedParameter[]) {
-  console.log('adding history');
   try {
     // Read the existing history
     let history: ParsedParameter[][] = await getHistory();
-    console.log('Current history before update:', history);
 
     // Add new parsedParams to the history
     history.unshift(parsedParams);
-    console.log('Updated history to be saved:', history);
 
     //Remove Duplicates
     history = history.filter(
@@ -25,11 +22,9 @@ export async function addHistory(parsedParams: ParsedParameter[]) {
   } catch (error) {
     console.error('Error adding history:', error);
   }
-  console.log('addingend history');
 }
 
 export async function getHistory(): Promise<ParsedParameter[][]> {
-  console.log('getting history');
   try {
     // Retrieve the history from client storage
     let history: ParsedParameter[][] | null = await figma.clientStorage.getAsync('figmautilshistory');
@@ -43,7 +38,6 @@ export async function getHistory(): Promise<ParsedParameter[][]> {
       (item1, index, array) => array.findIndex((item2) => JSON.stringify(item1) === JSON.stringify(item2)) === index
     );
 
-    console.log('history:', history);
     return history;
   } catch (error) {
     console.error('Error getting history:', error);
