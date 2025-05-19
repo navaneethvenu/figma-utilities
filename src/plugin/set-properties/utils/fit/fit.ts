25f3abe513eb3c28374af617771d5ae348785c81
Navaneeth Venu <69888461+navaneethvenu@users.noreply.github.com>
2025-05-19 08:10:43 +0530

    feat: add fit to width command

---
+++ src/plugin/set-properties/utils/fit/fit.ts
@@ -0,0 +1,51 @@
+import notifyError from '../../../utils/error';
+import { ErrorType } from '../../../utils/errorType';
+import { supportedNodes, SupportedNodes } from './supported-nodes';
+
+interface FitProps {
+  param: string;
+  nodes: readonly SceneNode[];
+}
+
+export function fitToParent({ param, nodes }: FitProps) {
+  for (const node of nodes) {
+    const nodeCheck = supportedNodes.find((type) => node.type === type);
+    const assertedNode = node as SupportedNodes;
+    const parent = assertedNode.parent;
+
+    if (!parent || parent === figma.currentPage || !('width' in parent) || !('height' in parent)) {
+      notifyError({
+        type: ErrorType.UNSUPPORTED_PROP,
+        message: 'Fit is not applicable on top-level nodes',
+      });
+      continue;
+    }
+
+    if (nodeCheck !== undefined) {
+      const parentWidth = (parent as SupportedNodes).width;
+      const parentHeight = (parent as SupportedNodes).height;
+
+      if (param === 'fit' || param === 'fitw') {
+        assertedNode.resize(parentWidth, assertedNode.height);
+        assertedNode.x = 0;
+      }
+      if (param === 'fit' || param === 'fith') {
+        assertedNode.resize(assertedNode.width, parentHeight);
+        assertedNode.y = 0;
+      }
+
+      const message = {
+        fit: 'Fitted to parent width and height',
+        fitw: 'Fitted to parent width',
+        fith: 'Fitted to parent height',
+      }[param];
+
+      figma.notify(message);
+    } else {
+      notifyError({
+        type: ErrorType.UNSUPPORTED_PROP,
+        message: `Fit is not applicable on node type ${node.type}`,
+      });
+    }
+  }
+}
