5f2202d9bff7dec40f14410a835cb4dd75f2153a
Navaneeth Venu <69888461+navaneethvenu@users.noreply.github.com>
2025-05-29 08:32:17 +0530

    fix: add support for fit to parent for Groups

--- src/plugin/set-properties/utils/fit/supported-nodes.ts
+++ src/plugin/set-properties/utils/fit/supported-nodes.ts
@@ -1,9 +1,9 @@
 import { figjamNodes, FigjamNodes } from '../figjam-nodes';
 import { sceneNode } from '../scene-node';
 
-type UnsupportedNodes = SliceNode | GroupNode | BooleanOperationNode | SectionNode | FigjamNodes;
+type UnsupportedNodes = SliceNode | BooleanOperationNode | SectionNode | FigjamNodes;
 
-const unsupportedNodes: NodeType[] = ['SLICE', 'GROUP', 'BOOLEAN_OPERATION', 'SECTION', ...figjamNodes];
+const unsupportedNodes: NodeType[] = ['SLICE', 'BOOLEAN_OPERATION', 'SECTION', ...figjamNodes];
 
 export type SupportedNodes = Exclude<SceneNode, UnsupportedNodes>;
 
